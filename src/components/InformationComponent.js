305df4da2db30ebd3b179798a2161e1acfc4a1ec
jdbecerra <jdbecerra1202@users.noreply.github.com>
2021-10-24 11:43:49 -0500

    created pages

---
+++ src/components/InformationComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const InformationComponent = () => {
+    return (
+        <section className="bg-primary">
+            <div className="container bg-primary">
+                e
+            </div>
+        </section>
+        
+    )
+}
